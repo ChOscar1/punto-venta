a87d750af15384ba8c31d45f30ca2b84f8ebaae2
ChOscar1 <oscarrrrrt@gmail.com>
2026-08-05 17:40:22 -0600

    Refactor de controller para flujo de Categoria

--- src/main/java/com/oscar/desarrollo/springboot/app/pos/puntoventa/modulo/application/CategoriaController.java
+++ src/main/java/com/oscar/desarrollo/springboot/app/pos/puntoventa/modulo/application/CategoriaController.java
@@ -11,9 +11,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/punto-venta")
+@RequestMapping("/punto-venta/categorias")
 @Slf4j
-public class PosController {
+public class CategoriaController {
 
     @Autowired
     CategoriaService categoria;
