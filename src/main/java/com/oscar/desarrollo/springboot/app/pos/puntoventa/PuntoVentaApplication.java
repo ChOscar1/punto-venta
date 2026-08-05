3f44538b5c6619bdb08bc2cee17fffaa352f867c
ChOscar1 <oscarrrrrt@gmail.com>
2026-08-05 13:30:22 -0600

    Proyecto inicial

---
+++ src/main/java/com/oscar/desarrollo/springboot/app/pos/puntoventa/PuntoVentaApplication.java
@@ -0,0 +1,13 @@
+package com.oscar.desarrollo.springboot.app.pos.puntoventa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuntoVentaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuntoVentaApplication.class, args);
+    }
+
+}
