5b01fa8219d3b750b21718b25d0b1570bc99557a
ChOscar1 <oscarrrrrt@gmail.com>
2026-08-11 15:30:00 -0600

    Corregir CORS

--- src/main/java/com/oscar/desarrollo/springboot/app/pos/puntoventa/config/ConfigCors.java
+++ src/main/java/com/oscar/desarrollo/springboot/app/pos/puntoventa/config/ConfigCors.java
@@ -12,7 +12,7 @@ public class ConfigCors implements WebMvcConfigurer {
 
         registry
                 .addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "https://punto-venta-front-gwkb.onrender.com")
                 .allowedMethods(
                         "GET",
                         "POST",
